b4e3584c3f3c5da341476ad9f3b42d4ab73c1311
jake bareng <barengjake@gmail.com>
2024-06-20 19:58:42 -0700

    img and text scroll

--- src/components/intro/headshot.tsx
+++ src/components/intro/headshot.tsx
@@ -1,7 +1,7 @@
 import headshot1 from '../../assets/headshot_1.png';
 import headshot2 from '../../assets/headshot_2.png';
 import headshot3 from '../../assets/headshot_3.png';
-import styles from "./headshot_styles.module.css" 
+import styles from "./headshot_styles.module.css"
 
 export default function Headshot() {
     const images = [
@@ -12,7 +12,13 @@ export default function Headshot() {
 
     return (
         <div className={styles.container}>
-            <img src={images[1]} alt="headshot" className={styles.image}/>
+            <div className={styles.img_collection}>
+                {
+                    images.map((image, index) => (
+                        <img className={styles.headshot} key={index} src={image} alt="headshot" />
+                    ))
+                }
+            </div>
         </div>
     )
 }
--- src/components/intro/textScrollEffect.tsx
+++ src/components/intro/textScrollEffect.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import styles from "./textScrollEffect.module.css"
 
 export default function TextScrollEffect() {
@@ -5,16 +6,55 @@ export default function TextScrollEffect() {
         "HTML5/CSS3",
         "JavaScript",
         "React",
-        "web developer",
+        "Web Developer",
+        "Typescript",
+        "Angular",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "PostgreSQL",
+        "Python",
     ]
+
+    function shuffleArray(array: number[]) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    let traversePath: number[] = []
+    for (let i = 0; i < texts.length; i++) {
+        traversePath.push(i)
+    }
+    shuffleArray(traversePath)
+
+    // timer 
+    useEffect(() => {
+        const textContainer = document.getElementById("textContainer")!
+        let textHeight = 42
+        let index = 0
+        const timer = setInterval(() => {
+            let y = textHeight * traversePath[index]
+            textContainer.style.transform = `translateY(-${y}px)`
+            index++
+            if (index === texts.length) {
+                index = 0
+                shuffleArray(traversePath)
+            }
+        }, 1700)
+
+        return () => clearInterval(timer)
+    }, [])
 
     return (
         <div className={styles.container}>
-            <div className={styles.container_text}
-            >
+            <div id="textContainer" className={styles.container_collection}>
                 {
                     texts.map((text, index) => (
-                        <h3 key={index}>{text}</h3>
+                        <h3 className={styles.text} key={index}>{text}</h3>
                     ))
                 }
             </div>
