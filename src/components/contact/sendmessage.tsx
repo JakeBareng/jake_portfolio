abd008bb23ab9c8f28d7ef994e941d1486b77216
jake bareng <barengjake@gmail.com>
2024-07-05 10:05:22 -0700

    error handling and submit success component

--- src/components/contact/sendmessage.tsx
+++ src/components/contact/sendmessage.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "@formspree/react"
+import { ValidationError, useForm } from "@formspree/react"
 import { useState } from "react";
 import styles from './sendmessage.module.css'
 
@@ -6,27 +6,37 @@ export default function SendMessage() {
     const [state, handleSubmit] = useForm(`${import.meta.env.VITE_FORMSPREE_ID}`);
     const [name, setName] = useState('');
 
-    if (state.succeeded) {
-        return (
-            <div>
-                <h1>Thanks {name} for reaching out!</h1>
-                <p>I'll get back to you as soon as possible.</p>
-            </div>
-        )
-    }
-
     return (
-            <section className={styles.container}>
-                <form className={styles.contact_form} onSubmit={handleSubmit}>
-                    <h1 className={styles.title}>Send Me A Message!</h1>
-                    <input placeholder="Your Name" type="text" name="name" value={name} onChange={e => setName(e.target.value)} />
-                    <input placeholder="name@email.com" type="email" name="email" />
-                    <input placeholder="Your Business" type="text" name="business" />
-                    <textarea placeholder="Your Message" name="message" />
-                    <button type="submit" disabled={state.submitting}>
-                        Send Message
-                    </button>
-                </form>
-            </section>
+        <section className={styles.container}>
+            <form className={styles.contact_form} onSubmit={handleSubmit}>
+                {
+                    state.succeeded ?
+                        <>
+                            <h1 className={styles.title}>Thanks {name} for reaching out!</h1>
+                            <p className={styles.title}>I'll get back to you as soon as possible.</p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p className={styles.title}>Jake Bareng.</p>
+                        </>
+                        :
+                        <>
+                            <h1 className={styles.title}>Send Me A Message!</h1>
+                            <input id="name" placeholder="Your Name" type="text" name="name" value={name} onChange={e => setName(e.target.value)} />
+                            <ValidationError className={styles.error} prefix="name" field="name" errors={state.errors} />
+                            <input id="email" placeholder="name@email.com" type="email" name="email" />
+                            <ValidationError className={styles.error} prefix="email" field="email" errors={state.errors} />
+                            <input placeholder="Your Business" type="text" name="business" />
+                            <textarea placeholder="Your Message" name="message" />
+                            <ValidationError className={styles.error} errors={state.errors} />
+                            <button type="submit" disabled={state.submitting}>
+                                Send Message
+                            </button>
+                        </>
+                }
+            </form>
+        </section>
     )
 }
