e9ac6223f09455bff258dd1adb3f8ff20f28db8e
jake bareng <barengjake@gmail.com>
2024-07-09 13:47:53 -0700

    linting

--- src/components/myServices/myservices.tsx
+++ src/components/myServices/myservices.tsx
@@ -43,26 +43,6 @@ export default function MyServices() {
         }
     ]
 
-    const container = {
-        hidden: { opacity: 1, scale: 0 },
-        visible: {
-            opacity: 1,
-            scale: 1,
-            transition: {
-                delayChildren: 0.3,
-                staggerChildren: 0.1
-            }
-        }
-    };
-
-    const item = {
-        hidden: { y: 20, opacity: 0 },
-        visible: {
-            y: 0,
-            opacity: 1
-        }
-    };
-
 
     return (
         <motion.section
