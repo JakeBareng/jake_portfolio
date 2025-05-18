70349b5cf6d9b2e0c5e6ace05170802682a8a17e
jakebareng <barengjake@gmail.com>
2025-05-17 19:19:11 -0700

    removed photos from profile

--- src/components/intro/headshot.tsx
+++ src/components/intro/headshot.tsx
@@ -6,17 +6,11 @@ import { motion } from "framer-motion"
 
 export default function Headshot() {
     const images = [
-        headshot1,
         headshot2,
-        headshot3,
     ]
 
     return (
         <motion.div
-            transition={{
-                type: "spring",
-                duration: 1
-            }}
             whileHover={
                 {
                     scale: 1.1,
@@ -28,27 +22,11 @@ export default function Headshot() {
                 }
             }
             className={styles.container}>
-
-            <motion.div
-                animate={{
-                    opacity: 1,
-                    x: [0,"-100%", "-200%"], 
-                }}
-                transition={{
-                    duration: 4,
-                    repeat: Infinity,
-                    type: "linear",
-                    repeatType: "reverse",
-                    delay: 1,
-                    repeatDelay: 1
-                }} 
-            className={styles.img_collection}>
-                {
-                    images.map((image, index) => (
-                        <img className={styles.headshot} key={index} src={image} alt="headshot" />
-                    ))
-                }
-            </motion.div>
+            {
+                images.map((image, index) => (
+                    <img className={styles.headshot} key={index} src={image} alt="headshot" />
+                ))
+            }
         </motion.div>
     )
 }
