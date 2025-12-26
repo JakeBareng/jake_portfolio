f83a240527892887881981f13f3e9a8496c3dfef
jakebareng <barengjake@gmail.com>
2025-12-25 20:54:59 -0800

    removed unecessary sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import './App.css'
 import Intro from './components/intro/intro'
 import About from './components/about/about'
-import MyServices from './components/myServices/myservices'
-import TechStack from './components/techstack/techstack'
 import Navbar from './components/navbar/navbar'
 import Projects from './components/projects/projects'
 import Contact from './components/contact/contact'
 
 function App() {
-  return (
-    <main>
-      <Navbar/>
-      <Intro/>
-      <About/>
-      <TechStack/> 
-      <MyServices/>
-      <Projects/>
-      <Contact/> 
-    </main>
-  )
+    return (
+        <main>
+            <Navbar/>
+            <Intro/>
+            <About/>
+            <Projects/>
+            <Contact/>
+        </main>
+    )
 }
 
 export default App
--- src/components/projects/project_list.ts
+++ src/components/projects/project_list.ts
@@ -2,8 +2,16 @@ import music_app from "../../assets/images/project_images/music_app.png"
 import chatroom_app from "../../assets/images/project_images/chatroom_app.png"
 import DJ_landing_page from "../../assets/images/project_images/DJ_landing_page.png"
 import ProjectType from "../../types/projectList.ts";
+import foodconnect from "../../assets/images/project_images/foodconnect.png"
 
 export const projects: ProjectType[]  = [
+    {
+        img_url: foodconnect,
+        title: "Food Connect",
+        body: "FoodConnect is a web platform that bridges the gap between food donors and recipients. It provides a marketplace for food donations, AI-powered food expiry predictions, a real-time chat system, and various filtering options to ensure food accessibility for those in need.",
+        tags:['React', 'Tailwind', 'CSS', 'TypeScript', 'JavaScript', 'HTML', 'Express.js', 'AWS', 'PostgreSQL'],
+        link:"https://github.com/Priyanshsarvaiya/Food-Connect"
+    },
     {
         img_url: DJ_landing_page,
         title: "DJ Landing Page",
